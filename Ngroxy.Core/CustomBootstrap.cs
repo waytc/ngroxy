dd4230cccefba5e11d1e0312eccc4a6e05218f32
mokeyish <mokeyish@hotmail.com>
2017-03-20 19:27:36 +0800

    add CustomBootStrap

---
+++ Ngroxy.Core/CustomBootstrap.cs
@@ -0,0 +1,50 @@
+﻿#region summary
+
+//   ------------------------------------------------------------------------------------------------
+//   <copyright file="CustomBootstrap.cs">
+//     用户：朱宏飞
+//     日期：2017/03/20
+//     时间：19:12
+//   </copyright>
+//   ------------------------------------------------------------------------------------------------
+
+#endregion
+
+namespace Ngroxy
+{
+    using System.Configuration;
+    using System.Net;
+    using System.Net.Sockets;
+    using DotNetty.Common.Internal.Logging;
+    using DotNetty.Handlers.Logging;
+    using DotNetty.Transport.Bootstrapping;
+    using DotNetty.Transport.Channels;
+    using DotNetty.Transport.Channels.Sockets;
+    using Microsoft.Extensions.Logging;
+    using Ngroxy.Handlers.Socks;
+    using NLog.Extensions.Logging;
+
+    public class CustomBootstrap : ServerBootstrap
+    {
+        private static readonly ILogger Logger = InternalLoggerFactory.DefaultFactory.GetCurrentClassLogger();
+
+        private readonly MultithreadEventLoopGroup _bossGroup = new MultithreadEventLoopGroup();
+        private readonly MultithreadEventLoopGroup _workerGroup = new MultithreadEventLoopGroup();
+
+        public CustomBootstrap()
+        {
+            Group(_bossGroup, _workerGroup);
+            ChannelFactory(() => new TcpServerSocketChannel(AddressFamily.InterNetwork));
+            Option(ChannelOption.SoBacklog, 1024);
+            Handler(new LoggingHandler("Service-listen"));
+            ChildHandler(new ActionChannelInitializer<ISocketChannel>(channel => { channel.Pipeline.AddLast(new SocksServerHandler()); }));
+        }
+
+        public void Run()
+        {
+            var port = int.Parse(ConfigurationManager.AppSettings.Get("port"));
+            BindAsync(new IPEndPoint(IPAddress.Any, port)).Wait();
+            Logger.LogInformation($"Server has started in port {port}");
+        }
+    }
+}
--- Ngroxy/Program.cs
+++ Ngroxy/Program.cs
@@ -3,48 +3,23 @@
 namespace Ngroxy
 {
     using System;
-    using System.Configuration;
     using System.IO;
-    using System.Net;
-    using System.Net.Sockets;
     using DotNetty.Common.Internal.Logging;
-    using DotNetty.Handlers.Logging;
-    using DotNetty.Transport.Bootstrapping;
-    using DotNetty.Transport.Channels;
-    using DotNetty.Transport.Channels.Sockets;
-    using Microsoft.Extensions.Logging;
-    using Ngroxy.Handlers.Socks;
     using NLog.Extensions.Logging;
 
     internal class Program
     {
-        private static readonly ILogger Logger = InternalLoggerFactory.DefaultFactory.CreateLogger<Program>();
-
         static Program()
         {
             var dbFile = new FileInfo(AppDomain.CurrentDomain.BaseDirectory + "/Data");
             AppDomain.CurrentDomain.SetData("DataDirectory", dbFile.FullName);
             InternalLoggerFactory.DefaultFactory.AddNLog();
         }
-
-        private static readonly MultithreadEventLoopGroup BossGroup = new MultithreadEventLoopGroup();
-        private static readonly MultithreadEventLoopGroup WorkerGroup = new MultithreadEventLoopGroup();
-
+        
         private static void Main(string[] args)
         {
-            var bootstrap = new ServerBootstrap();
-            bootstrap.Group(BossGroup, WorkerGroup);
-            bootstrap.ChannelFactory(() => new TcpServerSocketChannel(AddressFamily.InterNetwork));
-            bootstrap.Option(ChannelOption.SoBacklog, 1024);
-            bootstrap.Handler(new LoggingHandler("SRV-LSTN"));
-            //                bootstrap.Handler(new CheckHandler());
-            bootstrap.ChildHandler(new ActionChannelInitializer<ISocketChannel>(channel =>
-            {
-                channel.Pipeline.AddLast(new SocksServerHandler());
-            }));
-            var port = int.Parse(ConfigurationManager.AppSettings.Get("port"));
-            bootstrap.BindAsync(new IPEndPoint(IPAddress.Any, port)).Wait();
-            Logger.LogInformation($"Server has started in port {port}");
+            var bootstrap = new CustomBootstrap();
+            bootstrap.Run();
             Console.ReadKey();
       }
     }
